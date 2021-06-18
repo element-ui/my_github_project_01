cb364806d3fb1980271ea18c1a9cb370b42a2a31
陈楚义 <2967911782@qq.com>
2021-06-18 09:07:08 +0800

    gan

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-    <el-row>
-      <el-button icon="el-icon-search" circle></el-button>
-      <el-button type="primary" icon="el-icon-edit" circle></el-button>
-      <el-button type="success" icon="el-icon-check" circle></el-button>
-      <el-button type="info" icon="el-icon-message" circle></el-button>
-      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle></el-button>
-    </el-row>
     <img src="./assets/logo.png">
     <div>
       <p>
